13cea6967186510ffca5d1e52c00fdff76984ccd
Jason Dogariu <jdogariu@gmail.com>
2016-03-23 03:59:06 -0400

    update

--- mashups.js
+++ mashups.js
@@ -25,7 +25,7 @@ var reconnect = function() {
 	client.connect(creds).then(function() {
 		connected = true;
 		console.log("CONNECTED");
-		console.log(queued.msgs.length + " MESSAGES WERE QUEUED");
+		console.log(queued_msgs.length + " MESSAGES WERE QUEUED");
 		for (var i = 0; i < queued_msgs.length; i++) {
 			var user = queued_msgs[i].user || "Unknown";
 			var msg = queued_msgs[i].msg || "";
